2b8bb0875fdee3e3518342244bb2640860e3ae61
Farhan1013 <Farhan1013@gmail.com>
2015-04-07 16:40:18 -0700

    color toggle and range selector

--- js/app.js
+++ js/app.js
@@ -13,18 +13,26 @@ $(".controls li").click(function(){
   color = $(this).css("background-color");
 });
 
-
-  //cache current color
-
   
 //When "New Color" is pressed
+$("#revealColorSelect").click(function(){
+	 //Show color select or hide the color select
+	 changeColor();
+	 $("#colorSelect").toggle();
 
-  //Show color select or hide the color select
-
+});
 
 //update the new color span
+ function changeColor() {
+ 	var r = $("#red").val();
+ 	var g = $("#green").val();
+ 	var b = $("#blue").val();
+ 	$("#newColor").css("background-color", "rgb(" + r + "," + g + "," + b + " )");
+
+ }
 
 //When color sliders change
+$("input[type=range]").change(changeColor);
 
 
 //When "Add Color" is pressed
