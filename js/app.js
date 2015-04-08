333e9d328ae4b84d6c2b5db59aa2fcd4f86a373c
Farhan1013 <Farhan1013@gmail.com>
2015-04-07 21:32:24 -0700

    drawing fucntion added

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 //Solution: When user interacts cause changes appropriately
 
 var color = $(".selected").css("background-color");
+var $canvas = $('canvas');
+var context = $canvas[0].getContext("2d");
+var lastEvent;
 
 //When clicking on control list items
 $(".controls").on("click", "li", function(){
@@ -41,19 +44,29 @@ $("#addNewColor").click(function(){
 	  var $newColor= $("<li></li>");
 	  $newColor.css("background-color", $("#newColor").css("background-color"));
 	  $(".controls ul").append($newColor);
-  //Select the new color
+  	  //Select the new color
   	  $newColor.click();
 
 });
 
-  //Append the color to the controls ul
-
-  //Select the new color
-
-
 //On mouse events on the canvas
+$canvas.mousedown(function(e){
+	lastEvent = e;
+	mouseDown = true;
+}).mousemove(function(e){
+	  //Draw lines
+	  if(mouseDown){
+		  context.beginPath();
+		  context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+		  context.lineTo(e.offsetX, e.offsetY);
+		  context.strokeStyle = color;
+		  context.stroke();
+		  lastEvent = e;
+	}
+}).mouseup(function(){
+	mouseDown = false;
+})
 
-  //Draw lines
 
 
 
